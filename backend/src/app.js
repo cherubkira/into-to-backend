99d2fd6e76259496a0db9a5816056936abca64ee
cherubkira <kirubelmelaku2019@gmail.com>
2025-12-11 13:10:06 +0300

    feat:add CRUD operation API

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import userRouter from './routes/user.route.js';
+import postRouter from './routes/post.route.js';
 
 
 const app = express(); // create an express app
@@ -8,6 +9,7 @@ app.use(express.json()); //middleware to parse json
 //routes declaration
 
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/posts', postRouter);
 
 
 //example route http://localhost:5000/api/v1/users/register
